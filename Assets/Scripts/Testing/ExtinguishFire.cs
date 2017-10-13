bfd4197c8559750af4c004f1d473b918573b95e3
Adrian Schollmeyer <efcuser@efclassroom.idmt.fraunhofer.de>
2017-10-13 16:18:35 +0200

    Add Fire extinguishing system

---
+++ Assets/Scripts/Testing/ExtinguishFire.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExtinguishFire : MonoBehaviour {
+
+    public int Timer = 1000;
+    public GameObject fire;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	/*void Update () {
+		
+	}*/
+
+    public void OnTriggerEnter (Collider coll)
+    {
+        Debug.Log("trigger");
+    }
+
+    public void OnParticleCollision (GameObject other)
+    {
+        Debug.Log("collision");
+        if (other == fire)
+        {
+            Debug.Log(Timer);
+            if (--Timer == 0)
+            {
+                fire.SetActive(false);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Testing/ParticleController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class ParticleController : MonoBehaviour
+{
+    public KeyCode triggerKey;
+
+    // Use this for initialization
+    void Start()
+    {
+        //emitter = EmittingObject.GetComponent<ParticleSystem>();
+        //EmittingObject.GetComponent<ParticleSystem>().Stop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.KeypadEnter) || Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(triggerKey))
+        {
+            GetComponent<ParticleSystem>().Play();
+        }
+        if (Input.GetKeyUp(KeyCode.KeypadEnter) || Input.GetKeyUp(KeyCode.Return) || Input.GetKeyUp(triggerKey))
+        {
+            GetComponent<ParticleSystem>().Stop();
+        }
+    }
+}
