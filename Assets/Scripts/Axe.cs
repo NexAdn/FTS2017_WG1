a90d0b2144f20cefa9a7d4c7666be04f47bde051
Aaron Weiß <efcuser@efclassroom.idmt.fraunhofer.de>
2017-10-13 15:20:17 +0200

    preliminary test wall scene

---
+++ Assets/Scripts/Axe.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Axe : MonoBehaviour
+{ 
+    
+    public float PickMoveSpeed;
+
+    private Vector3 orgPosition;
+    private void Start()
+    {
+        orgPosition = transform.position;
+    }
+
+    public void Update()
+    {
+        transform.position = transform.position + new Vector3(PickMoveSpeed, 0, 0);
+
+        if (transform.position.x < -0.5f)
+        {
+            transform.position = orgPosition;
+        }
+    }
+}
---
+++ Assets/Scripts/Wall.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+    public int HealthLevel = 4;
+
+    public void OnTriggerEnter(Collider collider)
+    {
+       if (collider.tag == "Axe")
+       {
+            var axeRigidbody = collider.gameObject.GetComponent<Rigidbody>();
+
+            Damage(axeRigidbody.velocity.magnitude);
+       }
+    }
+
+    private void Damage(float axeSpeed)
+    {
+        if (HealthLevel > 1)
+        {
+            if (axeSpeed >= 0f)
+            {
+                Debug.Log("Damage " + axeSpeed);
+
+                HealthLevel -= 1;
+                Debug.Log(HealthLevel);
+
+                GetComponent<MeshRenderer>().material = Resources.Load("breakableWall_" + HealthLevel) as Material;
+            }    
+        } else
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
