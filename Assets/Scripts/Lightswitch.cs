a63fed9ada5f47a08ecbea673f96414ea0c3e293
Adrian Schollmeyer <efcuser@efclassroom.idmt.fraunhofer.de>
2017-10-14 14:39:41 +0200

    Add lightswitch script

---
+++ Assets/Scripts/Lightswitch.cs
@@ -0,0 +1,32 @@
+﻿namespace VRTK.Examples
+{
+    using UnityEngine;
+    using UnityEngine.UI;
+
+    public class UI_Keyboard : MonoBehaviour
+    {
+        //private InputField input;
+
+        public GameObject TopWallText;
+        public GameObject RoomLight;
+        public void Enter()
+        {
+            //input.text = "";
+            TopWallText.SetActive(!TopWallText.activeSelf);
+            RoomLight.SetActive(!TopWallText.activeSelf);
+        }
+
+        private void Start()
+        {
+            //input = GetComponentInChildren<InputField>();
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyUp(KeyCode.KeypadEnter) || Input.GetKeyUp(KeyCode.Return))
+            {
+                Enter();
+            }
+        }
+    }
+}
